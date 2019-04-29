99f44a26f3ad063e9f8505710e658b73ef69b6ef
Ramirez, Oswald <Oswald.Ramirez@nexteraenergy.com>
2019-04-28 22:43:14 -0400

    Email Collection

--- win32_scan_emails.py
+++ win32_scan_emails.py
@@ -1,10 +1,27 @@
 import win32com.client
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+
+def print_score(scores):
+    for k in sorted(scores):
+        print('{0}: {1}, '.format(k, scores[k]), end="\n")
+
+
+def get_scores(sia, message_text):
+    return sia.polarity_scores(message_text)
+
+
+
+
 session = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 inbox = session.GetDefaultFolder(6)
 print(len(inbox.Items))
 message = inbox.Items
 
 body_sender = ''
+
+sia = SentimentIntensityAnalyzer()
 
 for msg in message:
     try:
@@ -13,3 +30,5 @@ for msg in message:
         body_sender = ''
 
     print(body_sender, msg.Subject, msg.Body)
+    scores = get_scores(sia, msg.Body)
+    print_score(scores)
